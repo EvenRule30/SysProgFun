01745fc1d04dbfb78a4a98a64fef62b360fe0408
EvenRule30 <evenrule30@gmail.com>
2025-12-14 22:44:47 -0800

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * fortune.c
+ * Author: Tyler Carter Mills
+ * Date: 12/14/2025
+ * Prints a fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Our hearts are restless until they find rest in you.\n");
+    return 0;
+}
