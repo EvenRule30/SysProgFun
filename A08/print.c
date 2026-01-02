c518009e6896568b23c9b20fa3f10719ee510d47
Tyler Carter Mills <evenrule30@gmail.com>
2026-01-01 18:02:04 -0800

    assignment complete

--- A08/print.c
+++ A08/print.c
@@ -5,6 +5,11 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
-
+  void *p = sbrk(0);
+  printf("The first is %p\n", p);
+  void *q = sbrk(0);
+  printf("The second is %p\n", q);
+  long x = ((long)q)-(long)p;
+  printf("Increased by %ld (%lx) bytes\n", x, x);
   return 0 ;
 }
